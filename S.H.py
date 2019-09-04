9ffe0b6444d2a631ec1b10475fc5fd149f1a8e69
manalais <manalais@berea.edu>
2019-09-04 10:19:37 -0400

    Intro

---
+++ S.H.py
@@ -0,0 +1,36 @@
+import turtle
+wn=turtle.Screen()
+s_turtle = turtle.Turtle()
+s_turtle.color('purple')
+s_turtle.pensize(5)
+s_turtle.circle(-120, -180)
+s_turtle.right(180)
+s_turtle.left(180)
+s_turtle.circle(180,-120)
+h_turtle = turtle.Turtle()
+h_turtle.color('blue')
+h_turtle.pensize(5)
+h_turtle.right(90)
+h_turtle.forward(180)
+h_turtle.left(180)
+h_turtle.forward(90)
+h_turtle.right(90)
+h_turtle.forward(90)
+h_turtle.left(90)
+h_turtle.forward(90)
+h_turtle.left(180)
+h_turtle.forward(180)
+
+
+
+
+
+
+
+
+
+
+
+
+
+wn.exitonclick()
